72d522c77da026f7ceac3b32a353cd25604a154c
汤思远 <cora_0903@outlook.com>
2023-11-09 21:52:32 +0800

    init
    bug:驼峰转换不生效导致SQL错误

--- src/main/java/com/cora/fastbi/mapper/ChartMapper.java
+++ src/main/java/com/cora/fastbi/mapper/ChartMapper.java
@@ -7,7 +7,6 @@ import com.cora.fastbi.model.entity.Chart;
 * @author Cora
 * @description 针对表【chart(图表信息表)】的数据库操作Mapper
 * @createDate 2023-11-09 17:47:19
-* @Entity com.yupi.springbootinit.model.entity.Chart
 */
 public interface ChartMapper extends BaseMapper<Chart> {
 
--- src/main/java/com/cora/fastbi/mapper/UserMapper.java
+++ src/main/java/com/cora/fastbi/mapper/UserMapper.java
@@ -7,7 +7,6 @@ import com.cora.fastbi.model.entity.User;
 * @author Cora
 * @description 针对表【user(用户)】的数据库操作Mapper
 * @createDate 2023-11-09 17:47:19
-* @Entity com.yupi.springbootinit.model.entity.User
 */
 public interface UserMapper extends BaseMapper<User> {
 
